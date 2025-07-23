116d4263a1b1cad672acfc4b6fa413346d007e58
Nikhil Gaba <nikhilkumargaba@gmail.com>
2025-07-23 21:56:27 +1000

    visualising non-linear relationships using ggplot

--- script_for_analysis.R
+++ script_for_analysis.R
@@ -11,3 +11,24 @@ library(AER)
 #Understand the nature of the data (columns, a few data rows, etc.)
 names(mydata)
 dim(mydata)
+
+# Visualising the relationships between Earnings with Years of Schooling and Work Experience
+# Construct the variable "Earnings"
+mydata$earnings = mydata$wage * mydata$hours/1000
+
+# Construct the scatter plots appropriately for each relationship titled and axes labelled, output in jpeg (image format)
+pdf("earnings_education_qm.pdf")
+ggplot(data=mydata, mapping = aes(x=school, y=earnings) #scatter plot with earnings as the dependent variable and school as the independent variable
+) + geom_point() + geom_smooth(formula = y ~ poly(x, 2)) + labs(
+  title="Relationship between Earnings ($1000s) and Education (in Years)", #title for scatter plot
+  x="Education (In Years)", #label for x-axis
+  y="Earnings (in $1000s)") #label for y-axis
+dev.off()
+
+pdf("earnings_experience_qm.pdf")
+ggplot(data=mydata, mapping = aes(x=exper, y=earnings) #scatter plot with earnings as the dependent variable and exper as the independent variable
+) + geom_point() + geom_smooth(formula = y ~ poly(x, 2)) + labs(
+  title="Relationship between Earnings (in $1000s) and Workforce Experience (in Years)", #title for scatter plot
+  x="Workforce Experience (in Years)", #label for x-axis
+  y="Earnings (in $1000s)") #label for y-axis
+dev.off()
